b649dc397ede110a8059168673afa8bc62dda6b0
bryanosorio2003 <bsosorio550@misena.edu.co>
2021-10-08 15:16:30 -0500

    se agregaron algunas funciones para identificar la edad del individuo y dar una respuesta correcta

--- app.js
+++ app.js
@@ -6,18 +6,30 @@ const response = document.querySelector('#p-response');
 
 button_primary.addEventListener('click',calculating_response);
 button_primary.addEventListener('click',calculate_negative);
+button_primary.addEventListener('click',empty_field);
+button_primary.addEventListener('click',excessive_age);
 
 function calculating_response (){
 
     if (parseInt(age1.value) >=18){
-        response.textContent = name1.value +  ' SI PUEDE VOTAR';
+        response.textContent = name1.value +  ' si puede votar.';
     }
+    
     else{
-        response.textContent = name1.value + ' NO PUEDE VOTAR MMGV';
+        response.textContent = name1.value + ' no puede votar.';
     }
 }
 function calculate_negative (){
     if (age1.value <0){
         response.textContent = 'El numero es erroneo, por favor indique un valor correcto.';
+    }
+}
+function empty_field(){
+    if (age1.value == '')
+        response.textContent = 'El campo está vacio, por favor digite el campo.';
+}
+function excessive_age(){
+    if (parseInt(age1.value) >=120){
+        response.textContent =   'El individuo ' + name1.value + ' podría haber muerto, por favor verifique su edad.';
     }
 }
